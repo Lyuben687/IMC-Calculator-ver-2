5ebc2263f177e3eda12ea74aca1b695bf89af759
Lyuben687 <luis-6871@hotmail.com>
2025-06-02 21:46:45 +0000

    feat: adiciona integração com Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsGnhPz0p3cPV8kd5iNFadJtuTDCxURro",
+  authDomain: "imc-calculadora-d03b9.firebaseapp.com",
+  projectId: "imc-calculadora-d03b9",
+  storageBucket: "imc-calculadora-d03b9.firebasestorage.app",
+  messagingSenderId: "338561932415",
+  appId: "1:338561932415:web:107a49312a01e098b0334a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
